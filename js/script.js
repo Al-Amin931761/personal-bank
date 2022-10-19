a7b2d7a628efce2f8f9fb0d0797335adc9270fa5
Al Amin <alamin931761@gmail.com>
2022-10-19 21:49:15 +0600

    withdraw

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let depositValue = document.getElementById("deposit-value");
+let withdrawValue = document.getElementById("withdraw-value");
 let depositOutput = document.getElementById('deposit-output');
+let withdrawOutput = document.getElementById('withdraw-output');
 let balanceOutput = document.getElementById('balance-output');
 
+// deposit 
 function deposit() {
     // deposit value update
     depositOutput.innerText = Number(depositOutput.innerText) + Number(depositValue.value);
@@ -11,4 +14,16 @@ function deposit() {
 
     // clear input field
     depositValue.value = '';
+}
+
+// withdraw
+function withdraw() {
+    // withdraw value update 
+    withdrawOutput.innerText = Number(withdrawOutput.innerText) + Number(withdrawValue.value);
+
+    // balance value update 
+    balanceOutput.innerText = Number(balanceOutput.innerText) - Number(withdrawValue.value);
+
+    // clear input field 
+    withdrawValue.value = '';
 }
