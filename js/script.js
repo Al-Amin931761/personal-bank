7e38b84a2cf9fe3310c2181b7b2ac5a0613d6f7e
Al Amin <alamin931761@gmail.com>
2022-10-21 10:50:23 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
+// deposit 
 let depositValue = document.getElementById("deposit-value");
-let withdrawValue = document.getElementById("withdraw-value");
 let depositOutput = document.getElementById('deposit-output');
+// withdraw
+let withdrawValue = document.getElementById("withdraw-value");
 let withdrawOutput = document.getElementById('withdraw-output');
+// total balance 
 let balanceOutput = document.getElementById('balance-output');
+
+function addMoney(currentAmount, newAmount) {
+    return Number(currentAmount) + Number(newAmount);
+};
+
+function getMoney(currentAmount, newAmount) {
+    return Number(currentAmount) - Number(newAmount);
+};
 
 // deposit 
 function deposit() {
-    // deposit value update
-    depositOutput.innerText = Number(depositOutput.innerText) + Number(depositValue.value);
-
+    // deposit value update 
+    const totalDeposit = addMoney(depositOutput.innerText, depositValue.value);
+    depositOutput.innerText = totalDeposit;
     // balance value update 
-    balanceOutput.innerText = Number(balanceOutput.innerText) + Number(depositValue.value);
-
+    const totalBalance = addMoney(balanceOutput.innerText, depositValue.value);
+    balanceOutput.innerText = totalBalance;
     // clear input field
     depositValue.value = '';
 }
@@ -19,11 +30,11 @@ function deposit() {
 // withdraw
 function withdraw() {
     // withdraw value update 
-    withdrawOutput.innerText = Number(withdrawOutput.innerText) + Number(withdrawValue.value);
-
+    const totalWithdraw = addMoney(withdrawOutput.innerText, withdrawValue.value);
+    withdrawOutput.innerText = totalWithdraw;
     // balance value update 
-    balanceOutput.innerText = Number(balanceOutput.innerText) - Number(withdrawValue.value);
-
+    const totalBalance = getMoney(balanceOutput.innerText, withdrawValue.value);
+    balanceOutput.innerText = totalBalance;
     // clear input field 
     withdrawValue.value = '';
 }
